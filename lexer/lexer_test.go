31548dfa441ec166160053c25d99b78153a9322c
nitishsharma2825 <sharnit2825@gmail.com>
2024-11-04 19:13:14 +0530

    moved lexer code

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -40,36 +40,3 @@ func TestNextToken(t *testing.T) {
 		}
 	}
 }
-
-func (l *Lexer) NextToken() token.Token {
-	var tok token.Token
-
-	switch l.ch {
-	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
-	case ';':
-		tok = newToken(token.SEMICOLON, l.ch)
-	case '(':
-		tok = newToken(token.LPAREN, l.ch)
-	case ')':
-		tok = newToken(token.RPAREN, l.ch)
-	case ',':
-		tok = newToken(token.COMMA, l.ch)
-	case '+':
-		tok = newToken(token.PLUS, l.ch)
-	case '{':
-		tok = newToken(token.LBRACE, l.ch)
-	case '}':
-		tok = newToken(token.RBRACE, l.ch)
-	case 0:
-		tok.Type = token.EOF
-		tok.Literal = ""
-	}
-
-	l.readChar()
-	return tok
-}
-
-func newToken(tokenType token.TokenType, ch byte) token.Token {
-	return token.Token{Type: tokenType, Literal: string(ch)}
-}
