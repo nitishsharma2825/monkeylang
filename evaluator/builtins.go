4a6a41995cba0d73e2c1a63a765fe2b3c999350d
nitishsharma2825 <sharnit2825@gmail.com>
2024-11-19 19:43:47 +0530

    added support for puts builtin for printing to stdout

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "practice/nitish/monkey/object"
+import (
+	"fmt"
+	"practice/nitish/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -93,6 +96,15 @@ var builtins = map[string]*object.Builtin{
 			newElements := make([]object.Object, length+1)
 			copy(newElements, arr.Elements)
 			newElements[length] = args[1]
+
+			return NULL
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
 
 			return NULL
 		},
